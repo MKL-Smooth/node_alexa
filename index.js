73c20041f01679a25ac7059e84c478604e9579c5
MKL_Smooth <markuslessmann@gmx.de>
2018-02-06 14:26:58 +0100

    edit2 index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
     ,app = express()
-    ,http = require('http').Server(app); //do I need this?
-    ,io = require('socket.io')(http); //do I need this?
+    ,http = require('http').Server(app) //do I need this?
+    ,io = require('socket.io')(http) //do I need this?
     ,last_value;
 
 app.set('port', (process.env.PORT || 8080));
@@ -21,5 +21,5 @@ app.get('/', function (req, res) {
 })
 
 app.listen(app.get('port'), function () {
-  console.log("Node app is running on port', app.get('port')");
+  console.log('Node app is running on port', app.get('port'));
 })
